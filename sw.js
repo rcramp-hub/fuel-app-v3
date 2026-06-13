cfa54f898fed5d330c6bd879aa59ec90a7438cbe
rcramp-hub <ryan.cramp@outlook.com>
2026-06-13 17:47:42 +1000

    Updated sw file for user guide back button func

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'fuel-v1';
-const ASSETS = ['./index.html', './manifest.json', './icon.svg'];
+const CACHE = 'fuel-v1.2';
+const ASSETS = ['./index.html', './manifest.json', './icon.svg', './userguide.html'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
